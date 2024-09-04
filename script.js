4c121bc5b2ab9075057b52bce7a7390de7aca9b1
Fabree <reyescocafagardo@gmail.com>
2024-09-03 23:37:01 -0500

    actualizacion de los estilos de la pagina

---
+++ script.js
@@ -0,0 +1,12 @@
+// let theme = document.getElementById('theme');
+// let body = document.querySelector('body');
+
+// function cambiartema(){    
+//     if(theme.checked){
+//         body.style.background = "#09090b";
+//         body.style.color = "#fafafa";
+//     }else{
+//         body.style.background = "#fafafa";
+//         body.style.color = "#09090b";
+//     }
+// }
