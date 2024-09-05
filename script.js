d27a182aeffe297afafb55e0413ba6241694820a
Fabree <reyescocafagardo@gmail.com>
2024-09-04 23:54:18 -0500

    nuevas secciones

--- script.js
+++ script.js
@@ -10,3 +10,32 @@
 //         body.style.color = "#09090b";
 //     }
 // }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const fadeElements = document.querySelectorAll('.fade-in');
+  
+//     const handleScroll = () => {
+//       const viewportHeight = window.innerHeight-80;
+//       console.log(viewportHeight)
+//       fadeElements.forEach(element => {
+//         const rect = element.getBoundingClientRect();
+//         const elementTop = rect.top;
+//         const elementBottom = rect.bottom;
+        
+//         const visibleTop = Math.max(0, elementTop);
+//         const visibleBottom = Math.min(viewportHeight, elementBottom);
+        
+//         const visibleHeight = Math.max(0, visibleBottom - visibleTop);
+//         const elementHeight = elementBottom - elementTop;
+        
+//         let opacity = visibleHeight / elementHeight;
+//         opacity = Math.max(0, Math.min(1, opacity));
+        
+//         element.style.opacity = opacity;
+//       });
+//     };
+  
+//     window.addEventListener('scroll', handleScroll);
+//     handleScroll();
+//   });
+  
